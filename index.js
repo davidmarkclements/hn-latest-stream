6e18a6cff055c20f46731943add519188bfea838
David Mark Clements <david.mark.clements@gmail.com>
2020-08-08 19:13:23 +0200

    first

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict'
+const { promisify } = require('util')
+const stream = require('readable-stream')
+const got = require('got')
+
+const { PassThrough } = stream
+
+const base = 'https://hacker-news.firebaseio.com/v0'
+
+function htmlify (story) {
+  const { by, title, url } = story
+
+  return `
+  <article class='hn-article'>
+    <h2> ${by} </h2>
+    <h3>${title}<h3>
+    <a href="${url}">${url}</a>
+  </article>
+  `
+}
+
+async function hydrate (stream, max, json) {
+  const stories = await got(`${base}/newstories.json`).json()
+  if (json) stream.push('[')
+  for (const id of stories) {
+    const story = await got(`${base}/item/${id}.json`).json()
+    if (json) stream.push(JSON.stringify(story))
+    else stream.push(htmlify(story))
+    if (--max <= 0) break
+  }
+  stream.push(']')
+  stream.push(null)
+}
+
+
+// output may be "HTML" or "objects"
+// if objects then the stream is an object mode stream
+function hnLatestStream (max = 10, output = 'html') {
+  output = (output + '').toLowerCase()
+  if (output !== 'html' && output !== 'json') {
+    throw Error('output parameter must be "html" or "json"')
+  }
+  if (max <= 0) {
+    throw Error('max parameter must be greater than 0')
+  }
+  const json = output === 'json'
+  const stream = PassThrough()
+  
+  hydrate(stream, max, json).catch((err) => {
+    stream.emit('error', err)
+  })
+  return stream
+}
+
+
+
+module.exports = hnLatestStream
